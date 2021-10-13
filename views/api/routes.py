ded977e562a0e9d09be1ec761106e213cff427de
TheMoksej <saved.dat@gmail.com>
2021-10-14 01:20:14 +0200

    done for now, more updates tomorrow

--- views/api/routes.py
+++ views/api/routes.py
@@ -2,7 +2,7 @@ from discord import Embed
 from functools import wraps
 from contextlib import suppress
 from quart_rate_limiter import rate_limit
-from datetime import timedelta
+from datetime import timedelta, datetime
 from quart import Blueprint, jsonify, make_response, request, current_app
 
 from scripts.caching import Cache as cache
@@ -41,6 +41,8 @@ async def index():
 @rate_limit(3, timedelta(seconds=60))
 async def upvotes():
     # gonna finish this next time
+    print(request)
+    print(request.remote_addr)
     content = await request.get_json(force=True)
 
     user_id = content.get("user") or content.get("id") or content.get("uid") or content.get("User")
@@ -49,12 +51,11 @@ async def upvotes():
         user_id = user_id.get("ClientID") or user_id.get("id")
 
     user = await current_app.main_bot.fetch_user(int(user_id))
-    print(user)
-    embed = Embed(title="New vote received!", url="https://dredd-bot.xyz")
-    embed.set_image(url="https://media.discordapp.net/attachments/638902095520464908/659611283443941376/upvote.png?width=180&height=180")
+    embed = Embed(title="New vote received!", url="https://dredd-bot.xyz", color=0x5E82AC)
+    embed.set_thumbnail(url="https://media.discordapp.net/attachments/638902095520464908/659611283443941376/upvote.png?width=180&height=180")
     embed.set_author(name=user, icon_url=user.avatar_url)
+    embed.description = f"**{user}** has voted for me on **{datetime.utcnow()}**"
     channel = current_app.main_bot.get_channel(679647378210291832)
-    print((channel, embed.to_dict()))
     await channel.send(embed=embed)
 
     return await make_response({"message": "Success"}, 200)
